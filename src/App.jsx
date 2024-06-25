0ea429543edc4f62e4f1ac5420d16636c9eec244
Amitesh tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-06-25 11:01:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Header from './Components/Header'
+import NewsBoard from './Components/NewsBoard'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <Navbar />
+      <NewsBoard />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import img2 from"../assets/img2.png"
+import './NewsBoard.css'
+
+const NewsBoard = () => {
+  return (
+    <img src={img2} className="img-fluid" alt="..."/>
+  )
+}
+
+export default NewsBoard
