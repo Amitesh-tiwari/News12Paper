a72b681a1b124d1f186a62c8578e13affd0bc912
Amitesh-tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-08-07 18:49:19 +0530

    all changes done

--- src/Components/NewsBoard1.jsx
+++ src/Components/NewsBoard1.jsx
@@ -7,12 +7,67 @@ import './NewsBoard.css'
 
 function NewsBoard1() {
   return (
-    <div>
-        <img src = {image1} alt=''/>
-        <img src = {image2} alt=''/>
-        <img src = {image3} alt=''/>
-        <img src = {image4} alt=''/>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">News</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Fashion
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Lifestyle</a></li>
+              <li><a className="dropdown-item" href="#"> Features </a></li>
+              <li><hr className="dropdown-divider" /></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Gadgets
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Lifestyle</a></li>
+              <li><a className="dropdown-item" href="#"> Features </a></li>
+              <li><hr className="dropdown-divider" /></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Features
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Lifestyle</a></li>
+              <li><a className="dropdown-item" href="#"> Features </a></li>
+              <li><hr className="dropdown-divider" /></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" aria-disabled="false">Video</a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
     </div>
+    <div>
+        <img src= {image1} alt=''/>
+        <img src= {image2} alt=''/>
+        <img src= {image3} alt=''/>
+        <img src= {image4} alt=''/>
+    </div>
+  </nav>
+  
+  
+    
   )
 }
 
